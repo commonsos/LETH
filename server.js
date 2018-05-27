f785479b247a8777d32d209a726438c3aa5c57f0
junshin <j.kawasaki@giftedagent.com>
2018-05-27 20:12:07 +0900

    Force SSL

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express'),
     app = express();
 
-var forceSsl = require('force-ssl-heroku');
+var secure = require('ssl-express-www');
 
+app.use(secure);
 app.use(express.static('www'));
-app.use(forceSsl);
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
